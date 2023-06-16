cf2e5a05f3ca1c1f0e7748a642294d98368614ed
Christian Salazar <salazar3148@gmail.com>
2023-06-16 17:54:12 -0500

    initial commit

---
+++ src/main/java/com/pragma/powerup/messagingmicroservice/MessagingmicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.pragma.powerup.messagingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingmicroserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MessagingmicroserviceApplication.class, args);
+
+	}
+
+}
